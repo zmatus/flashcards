db9d44c75bb87956a7a4b6c83869374249b7534a
Ken Nguyen <nguyen.ken.v@gmail.com>
2020-08-26 21:03:40 -0400

    Generate flashcards from markdown file

--- _decks/deck-1.md
+++ _decks/deck-1.md
@@ -3,3 +3,12 @@ layout: card
 title: One random deck
 date: 2020-08-19
 ---
+
+1a. Some term here
+1b. The answer to the term above
+
+2a. Another term to study
+2b. The answer to the second term
+
+3a. Third term sits here
+3b. Explanation of the third term
--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,100 @@
-const decks = Array.from(document.querySelectorAll('.deck'));
-const search = document.querySelector('#search-decks');
+/**
+ * Filter out deck links from search query.
+ * 
+ * @param {HTMLElement} deckLinks
+ * @param {HTMLElement} searchBar
+ */
+function filterDecks(deckLinks, searchBar) {
 
-search.addEventListener('keyup', function(e) {
-  let query = e.target.value;
+  searchBar.addEventListener('keyup', function(e) {
+    let query = e.target.value;
+
+    deckLinks.forEach(deck => {
+      deck.classList.add('hide');
+    });
   
-  decks.forEach(deck => {
-    deck.classList.add('hide-deck');
-  })
-
-  decks.filter(deck => {
-    if (deck.textContent.toLowerCase().includes(query)) {
-      deck.classList.remove('hide-deck');
-    }
+    deckLinks.filter(deck => {
+      if (deck.textContent.toLowerCase().includes(query)) {
+        deck.classList.remove('hide');
+      }
+    });
   });
-});
+}
+
+if (window.location.pathname == '/') {
+  const decks = Array.from(document.querySelectorAll('.deck'));
+  const search = document.querySelector('#search-decks');
+  filterDecks(decks, search);
+}
+
+/**
+ * Create flashcard for each object inside array.
+ * 
+ * @param {HTMLElement} cardContainer 
+ * @param {Array.<Object>} termsArr 
+ */
+function makeCards(cardContainer, termsArr) {
+
+  for (let i = 0; i < termsArr.length; i++) {
+
+    let front = document.createElement('div');
+    front.classList.add('card-front');
+    front.textContent = termsArr[i].front;
+  
+    let back = document.createElement('div');
+    back.classList.add('card-back');
+    back.textContent = termsArr[i].back;
+  
+    let card = document.createElement('div');
+    card.classList.add('card');
+    card.appendChild(front);
+    card.appendChild(back);
+  
+    let cardBox = document.createElement('div');
+    cardBox.classList.add('card-box');
+    cardBox.appendChild(card);
+  
+    cardContainer.appendChild(cardBox);
+  }
+}
+
+/**
+ * Add event listener to flip card on click.
+ * 
+ * @param {HTMLElement} cards 
+ */
+function flipCard(cards) {
+
+  cards.forEach(card => {
+    card.addEventListener('click', function() {
+      card.classList.toggle('flipped');
+    });
+  });
+}
+
+if (window.location.pathname != '/') {
+  let container = document.querySelector('.cards-container');
+  let cardText = container.children;
+  
+  let allCards = [];
+  for (let i = 0; i < cardText.length; i++) {
+    let card = cardText[i];
+    let cardsArr = card.textContent.split('\n');
+
+    card.classList.add('hide');
+    
+    let front = cardsArr[0].split('.');
+    let back = cardsArr[1].split('.');
+    let cardObj = {
+      front: front[1].trim(),
+      back: back[1].trim()
+    };
+    allCards.push(cardObj);
+  }
+  console.log(allCards);
+
+  makeCards(container, allCards);
+
+  let cards = document.querySelectorAll('.card');
+  flipCard(cards);
+}
