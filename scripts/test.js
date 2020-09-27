6c07054c5831f1cac7a1fde7a4625fd438b1dd86
Ken Nguyen <nguyen.ken.v@gmail.com>
2020-09-27 12:02:21 -0400

    Remove additional test page

--- scripts/test.js
+++ scripts/test.js
@@ -23,114 +23,14 @@ const loadStorage = () => {
   return JSON.parse(localStorage.getItem('cards'));
 }
 
-/**
- * Hide nav when test starts.
- */
-const toggleNav = () => {
-  let nav = document.querySelector('.control');
-  nav.classList.toggle('hide');
-}
-
-/**
- * Clear container and show <nav>
- * when end test button is clicked.
- * 
- * @param {HTMLElement} end
- * @param {HTMLElement} container 
- */
-const clearContainer = (container) => {
-  while (container.firstChild) {
-    container.removeChild(container.lastChild);
-  }
-  toggleNav();
-}
-
-/**
- * Create test control buttons for simple test.
- * 
- * @param {HTMLElement} container 
- */
-const makeSimpleControl = (container) => {
-  let control = document.createElement('nav');
-  control.classList.add('simple-control');
-
-  let end = document.createElement('button');
-  end.innerHTML = '&larr; End Test';
-  end.classList.add('end-test');
-  control.appendChild(end);
-
-  let answer = document.createElement('button');
-  answer.innerHTML = '&#9873; Answer'
-  answer.classList.add('answer');
-  control.appendChild(answer);
-
-  let correct = document.createElement('button');
-  correct.innerHTML = '&#9745; Correct';
-  correct.classList.add('correct');
-  control.appendChild(correct);
-
-  let incorrect = document.createElement('button');
-  incorrect.innerHTML = '&#9746; Incorrect';
-  incorrect.classList.add('incorrect');
-  control.appendChild(incorrect);
-  
-  container.appendChild(control);
-}
-
-const makeSimpleBox = (container) => {
-  let front = document.createElement('div');
-  let frontHeader = document.createElement('h2');
-  frontHeader.textContent = 'Front';
-  front.classList.add('simple-front');
-  front.appendChild(frontHeader);
-  
-  let back = document.createElement('div');
-  let backHeader = document.createElement('h2');
-  backHeader.textContent = 'Back';
-  back.classList.add('simple-back');
-  back.appendChild(backHeader);
-  
-  let box = document.createElement('div');
-  box.classList.add('simple-test');
-  box.appendChild(front);
-  box.appendChild(back);
-  
-  container.appendChild(box);
-}
-
-/**
- * Generate questions
- * 
- * @param {Object} cards 
- */
-const generateSimpleTest = (container, cards) => {
-  console.log(cards);
-
-  makeSimpleControl(container);
-
-  // End test and go back to tests selection
-  let endTest = document.querySelector('.end-test');
-  endTest.addEventListener('click', () => {
-    clearContainer(container);
-  });
-
-  makeSimpleBox(container);
-}
-
 (function() {
-  let testContainer = document.querySelector('.test-container');
-
   // Go back to previous page
-  let finish = document.querySelector('#test-finish');
+  let finish = document.querySelector('#end-test');
   prevPage(finish);
-
+  
   // Load values from storage
   let cardsObj = loadStorage();
+  console.log(cardsObj);
 
-  // Start simple test
-  let simple = document.querySelector('#test-simple');
-  simple.addEventListener('click', () => {
-    toggleNav();
-    generateSimpleTest(testContainer, cardsObj);
-  });
+  let testContainer = document.querySelector('.test-container');
 })()
